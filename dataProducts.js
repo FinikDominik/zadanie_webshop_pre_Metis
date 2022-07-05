c419d1c6ba9b2b1545606046623cdc9aac5d059b
Dominik Fínik <92373597+FinikDominik@users.noreply.github.com>
2022-07-05 10:28:23 +0200

    Update dataProducts.js

--- dataProducts.js
+++ dataProducts.js
@@ -4,42 +4,42 @@ let ProduktData = [
       "trieda": "CD",
       "Nazov": "V Liptove Dedinka",
       "cena": "7",
-      "img": "https://photos.app.goo.gl/yRH5GJjJnF4RnvWs8"
+      "img": "https://lh3.googleusercontent.com/0YYcwKeh_h4hfOUO8wBCJlK5Lsnbd57YwpnqNf7o6VyrUoxm8NME5NzDxwlPbo2z6_DHd09VPWjQDP8O3f0lqIFFE_PhTeDIMKTbac95wqC49JpWIJj3dHlwuTyF-EEM7tUagjvCLByx12VfmM6bF8RuZ81bLM8SB2Gl8wOwe3QKCW9DUR2OtLKLWk8WrxwaUT9lljEAag_Q3igmkBFsmB8SozkUZGJhv-ge_JkctqEeT1OfjPjWYHs21On5b7we3hLa2d-KjjLNP8ZEAVayW-vaLcDiL9cMpzoebwRU2QqCq_gXMNEc52XbTikzq-aK5Gt9_RnQSeEDSR1zO_pOioYqeL7jJrWROqRsWYhPyJyqJNN4ykr0l7ZGYD7rbAyRZfMilWJ5H6l9qoXbRdXKQr3ijPPpuU1UZl1v3pYS4QWimFpYkZ_cf7v4tItlkHJOgtIF7ZjSIfC3lLHKYFsc30XiyWMqP_hHo3qQm_ALh2xb0LxBuS3wJR0dpbwqfPEmEFcCOJwU1Ya7fanMp552lXSzsB3yy-m1KEBGjdixhnrJ-AgaOIAarO6-gTG9WMd21Db4zSUKgdk_Z03SmEifVq2lt5gQdiO_vAxxqH6r_NBVh2GBau8oEi4Wm-fjaJlccRRmOhtyMJuFnEaRlDmB2KO25NlfPBfCrwsuQvp0jJId1zNC62JX1s8IQrmHMJ4vlMSEkOa5bLUd_a-jQ8rfY_HLOJNDt_4mLD0NVJEwTb5lagja_Ye7inXQ71fb4t2RQV9zYU6k1Osp2YRSJnOKHpvVdlmE2AZI73tqUbcsJNr3deyRxTVEBsr4EVNEFKFbR8g=w587-h582-no?authuser=0"
     },
     {
       "id": 2,
       "trieda": "CD",
       "Nazov": "Cez Černovú... PIESNE Z ČERNOVEJ",
       "cena": "7",
-      "img": "https://photos.app.goo.gl/emX1m6gxBrctoJzi7"
+      "img": "https://lh3.googleusercontent.com/zMx4iC0J-icH2J59U9QOLhaVKLKAOU3iapKEInnzUUtafv_FDg_uSEch5WqgwKsbvvz8OOySpejq3oOcAmSnTGca_vIzN6MCBCyoeaStBqf8tOMs4B_REkldjhExzsOq8ZUz2gQfO3OiVBiA5rrTr95QmwxXPjfA0lX2dv25poH770m_AIO-WWM-QjdgqKrZeCBr6cabuqFAeATeU1E97sb-n0Z8p3ytnu1YfbqYkWKiePzv7iCSsZXiB1XBw0bJBnk4wJ0dPQIPvJaSIa7JQZajt6S3qoKEdGhlC6i198akV1KHaMfD9QLXXce4yNWedE3rfPqvzB48WiPu_W6qI37L3UADPQohSVrXcw0LcFjOfoBU-iQMVE8pZFneht7_FBoX7MUJZacl3yvsS6JAM5epL8BVwruUU5o8XaL4e_Wp-RO5qEjKbsQDLt9D7HDI49pJpdLA9AL4UWLp4oYhc6qWFmpssNs-wfk2mZc10wMbdIu-AxwuW6EHSQjHRUDbF4XzUkeWvLZqrkjCuBK3mk35qrBYB5xkCgfN_4liL9PzMkZVC6PK3nisRjgkxvDyq-uG3IevYSpfETfAbAi9Fi7mQGuIgENsOqwpLnAykijYezmDtwLu9GeCetCnVXT1a27oe4CF_GWtIkBjPNAtf_Ww2T-M9ZFBManbly9SIeILtt3Aj77xNUTIWKZBrkYj658H20JzYSIk_xtvBZovdZGv0hCxrBK8zy5X00cb7V6xak-ValuFrni5fdDUcA83y_YtgvaMw2nnbb7HbKUoDWLAUsAxGL4jZhFe8hBWDJnPN3GHQU1q6sZNoVlC2YsXG2g=s587-no?authuser=0"
     },
     {
       "id": 3,
       "trieda": "CD",
       "Nazov": "HORY MOJE, HORY...",
       "cena": "7",
-      "img": "https://photos.app.goo.gl/nnax7iPEN65GvnRPA"
+      "img": "https://lh3.googleusercontent.com/9zikfvmHdQHhTU8kbrPFu_xW8oWiivJiy20D7iZqDbS_sh4u3nLBk-nP6xp-2PuWDJNeY5QewLLk7lEEItncvwg0ZXOhAl8mwL8cRHLIOo3pedHN1k-1ElPOJbwjCHCZpavCrxH_80AUEu7YOdz5kaDsH5dDnOe3532vKPPVwSyfWobRSjMVEkqZjp0TNS4dhJGG3l6-KZD0cBU4Q30mZB8MkGr0qrk1CO45oB6pbkRyjReqcFzZO0k7NH8YiNWP-R0pmWvq82DPE15jazYDqn1ZWm9SQB0EJAt4ZVg9mTqTcpKnSQ4-OzntV785KB9Cz1P0aOUEzxypV-EjTOFDlzOl06yL5_uNo-Ol0Njw2B-ZGFl5tm0lafAvoBFWTi6zmPfkGxIF8r09L5Foc_xJoUf4pAlIgfU6UM4izf1tNz_WXYKMwbb6YWweKXMFDRtPLCjGGWZRWt3WJu_i-kemWnFCzvgsgxfUOM8I7BYWOpTS6oXC3lLHU2IP4MpRlUCbqF4lZB6bUmP0one3nXn7vIx4yYwSIRLAPwOY059bDYtxXv73Y3cV7D1aur8P-9ovHhH9EesDdXiNtnF7JnlppDYpjRbM-BfWeKDm1S_LAzOaPMwzltOoJL9X-dI31IwMGEllu5O3NnIfb1oxujDMtQuLXqRCQdLwPhPdCkcRQt_mqQW8-H5S12e4XGRd1ewvXTkl67ZAm_M79NpotdgWLnpiQPkGYXCXoreMDM591i-qBbuLnBmzWENFuMhZenyP__aQnWYjLCtsOn2dwSvhx1A1uptq5TOzviZ4H49yFXocnzDNbuYmtyJmt6PVrYZwmpw=w587-h583-no?authuser=0"
     },
     {
       "id": 4,
       "trieda": "Kožené vyrobky",
       "Nazov": "Opasok trojprackový hnedý",
       "cena": "350",
-      "img": "https://photos.app.goo.gl/Jbw526MVUFs49TL38"
+      "img": "https://lh3.googleusercontent.com/uFNubrwWONTjkL9GXFDF7Av5-6_8EzwTRR0MaobSq9thrSRSz20LRW7h7XdTjbP3lZfDZsvoVeSzvLt5xRgCgwV2JDWDzQYroGcpMZGCVXdbjFtj-9pku3rFo1kKIzUsiGnZHeq8NZvJnMl9rs1wQAFhV_eV6g6G5_fLWqYA1O2QRggKjXPDwzkjFHoaktZG-N9DniqlauDRHufRB01POIQ875m0AyWQ8F-1PK-V51S_5SzbeTQ30HS66wFdrsJl1GqYqMSwCt-2zjKvQkHdUhkQjyhXKit837zGMN4jETrkn_6fIP0YFagvCiZJF0KL1DHyYubEbqdomr4BOv5RyoTqzR7lehO8kzpA55qkj5cflB5HfbiYq3H0gI3k9oSuHRjdAZt2VpNgjXQaKYFNuF915ExrH_AbsoXtPnmmsp1_IDXjEi_9Qpf6-uN0A9mm53ClXHis8etpvnPAjBOxhdoV_rRxi8DISlJq94mVzh9qP0M2XIdcqUOT7LdCytq2xytbSeezLu_quLjEK_wtWrQEuQ2iywfaE4rwZAQCInkIl5kStLk1kC8Jjh-rL8uAxQq0YE-lv7avIwOakKBFw7b9mnqe_2gXpDRjsRbEVSuSonUKjTV6Tmf7Erzd5Ih7VRidcBtZX5KpPkSvdQ0I4mnzFyW6yBKCyqOdpSQGT4rMEjjDhLvhXL2cebcaDR2FGEzS__L_TH0DktymR5HAtSOs6UHDMA1tki5HTfw7nEAzCogz7b6fMdt_X9FbkNN2rPJ4DNNdu0lm88t11JQKUmfvhPpospG9R8UcB6gfStNbgCTqp4qqH2fCoszf-Tpr7EE=w641-h477-no?authuser=0"
     },
     {
       "id": 5,
       "trieda": "Kožené vyrobky",
       "Nazov": "Opasok trojprackový 2",
       "cena": "350",
-      "img": "https://photos.app.goo.gl/TRGRwmygZeuc61qJ9"
+      "img": "https://lh3.googleusercontent.com/U2JlQsA9wVPCT33DbhM_2XcZmqM8i-QRIRv5qk4QIE8m5u845004jwLHAelhlY479hX4pYmTCBoQPmaiMFZzo-T-IPvRhyjUD6X5sgmSPhTpt5OW4NZJz9Bt_VHC9LHorkZlCJHOli3PX6L-VPqHBcvc9TCWpCP87O-3k29ufqyDSBol0utIlqYGL5MRK82tf1gg_y3Jyb7mhdJ5W3lYIZLYkprw0CM3tKbGEL2YE7QiB5psuYLn1829XjpBYOJ5CLlDCLoJbYgMuCyaCEka-YXNg8rqH2tGqwMyUenvramAWWdnWgms_uaZ5Valj6Scgg-stzuppr3LddlFf4j61nj6iozYhBvnIlX4tZi9c0sLlnOjIuZFm2CduFKtx4z8XgxY2OXh1nrn8Oi38t443jS4SMtUhfiAMTbwMp6ZfivfOWOnMY5PO5SJjkidlIxcldBqytLvTJUrERyqeXxLhfAOj35RTI2qtbXDjWX9SmwtiDRA1wDa1OheWbcI4G6y62XBkIeOzbwyDTnw0CelXkwdqIVqa4xMgU3Ky_fKB43ja7WR6bumM58O5WAyXeU4cKWoSzEmpGNJH6kER-UUnfAmJON8m_MdwaOXh3XCuxWBJdBUaqVpLcugJG4wyzx0q9Y8skyTJ_TtwKWtI2gxxteHWYplfA4jSa0oRFJGb-aHuZlSyIlWSBSeu6DHnzBnl2g-A_28NngH9uQcxmcVEyXwloS9wd1lqXBuZB1pBr6A5leGiomwosK7Iy_Cpg2gpAWLpjfoF1mic2bZ-FFR7PwSySG7FkZZRWEQcBWELEpCLS4IpvDtxGpFGkKXTHvrrps=w961-h721-no?authuser=0"
     },
     {
       "id": 6,
       "trieda": "Kožené vyrobky",
       "Nazov": "Opasok trojprackový 1",
       "cena": "350",
-      "img": "https://photos.app.goo.gl/BefXWiPQrtaiZzFp7"
+      "img": "https://lh3.googleusercontent.com/4KIKxaZzn-xd-H1dZ8jAT51sravcGorg8jTPN-ON3yInOhUt5bH39uHDaqgTS1mRf5HpSIWqxTjju4sM1v1-JK9WnzFpyaK6zBGi24PsrqMU4wf9Qa-p9zkUdgZQ0gpRN4Zn7qqJLrHl2XJLq1jgMgxCgVOFt0F5zZdQrsN6JGzPOEJb6iuyD6yLhZukpoYSKIe_XFjTHXFrErMorqL8QchFZFy2EpAXjQAvQ5l2GbXOrfEX1lD_mwWP4w9a5BT1gicNxNZfmvkn_1ycOcnv1BBoaVqB6qjJODCn3N-GVN2FRyRfrL5v2BjrsMMwLXaGdlx9GK6LnKcQKwFKVBLkQ_4sPLdb_lKimwtkgfLXBGgZAcjhiBMgIQszV0vr1fU8mMvE5TZK_nAuM6M9JWRO5i5Zg6cROdZt4D5v64hOywrxCDtmEDUxWtBGUidx_uHTXLWpdbgWfbnOGrLG0FjjRtzoYEttcMpZ9_hMkRjS2o0g44Vs9MEYzJFxKDrrbRt5rNbJUS6vEEBaqO9OOqdFpqqyEY8H5OmzXsfs2jKzSaql5uE1x6Yn8imApD6-oNFSYygXncZpU7AhNbEmwwq-iyK6EKzSShrR-w-cFzNzlXGZ1AX315CramEM1kw9SdD1AOrr0wZJrLug0BMrRSDJDRjkNJs8dgBJGa-26Oe1pc0SfQiPY0u4G1qdRMnEwJUM3axTs9nB_0fQY16LgJ6xbkxICM9UsF2r9j-KyNYHUNkWEzmh5YgzCHUBeAu3B-Ps9cet7iE_eyRxT3fWbjLyd561BXBETQ_wMhz8_gJk8d5UFZxywNMx1yh5fKKBZ7Zldlc=w456-h342-no?authuser=0"
     },
     {
       "id": 7,
