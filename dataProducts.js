d25ca5593edab4971181bbbadb475dfcc0a8379a
Dominik Fínik <92373597+FinikDominik@users.noreply.github.com>
2022-07-05 10:09:57 +0200

    Update dataProducts.js

--- dataProducts.js
+++ dataProducts.js
@@ -4,126 +4,126 @@ let ProduktData = [
       "trieda": "CD",
       "Nazov": "V Liptove Dedinka",
       "cena": "7",
-      "img": "img/Produkty/CD v liptove dedinka.png"
+      "img": "https://photos.app.goo.gl/yRH5GJjJnF4RnvWs8"
     },
     {
       "id": 2,
       "trieda": "CD",
       "Nazov": "Cez Černovú... PIESNE Z ČERNOVEJ",
       "cena": "7",
-      "img": "img/Produkty/CD Cez Černovú - piesne z Černovej.png"
+      "img": "https://photos.app.goo.gl/emX1m6gxBrctoJzi7"
     },
     {
       "id": 3,
       "trieda": "CD",
       "Nazov": "HORY MOJE, HORY...",
       "cena": "7",
-      "img": "img/Produkty/CD Hori moje hory....png"
+      "img": "https://photos.app.goo.gl/nnax7iPEN65GvnRPA"
     },
     {
       "id": 4,
       "trieda": "Kožené vyrobky",
       "Nazov": "Opasok trojprackový hnedý",
       "cena": "350",
-      "img": "img/Vyrobky z kože/Opasok 2.png"
+      "img": "https://photos.app.goo.gl/Jbw526MVUFs49TL38"
     },
     {
       "id": 5,
       "trieda": "Kožené vyrobky",
       "Nazov": "Opasok trojprackový 2",
       "cena": "350",
-      "img": "img/Vyrobky z kože/Opasok 4.png"
+      "img": "https://photos.app.goo.gl/TRGRwmygZeuc61qJ9"
     },
     {
       "id": 6,
       "trieda": "Kožené vyrobky",
       "Nazov": "Opasok trojprackový 1",
       "cena": "350",
-      "img": "img/Vyrobky z kože/Opasok 3.png"
+      "img": "https://photos.app.goo.gl/BefXWiPQrtaiZzFp7"
     },
     {
       "id": 7,
       "trieda": "Kožené vyrobky",
       "Nazov": "Opasok štvorprackový",
       "cena": "499",
-      "img": "img/Vyrobky z kože/opasok 5.png"
+      "img": "https://photos.app.goo.gl/W6KUvjzBLWFpkFTF8"
     },
     {
       "id": 8,
       "trieda": "Kožené vyrobky",
       "Nazov": "Opasok štvorprackový hnedý",
       "cena": "499",
-      "img": "img/Vyrobky z kože/Opasok 6.png"
+      "img": "https://photos.app.goo.gl/tm6rgyABUipFpjfE6"
     },
     {
       "id": 9,
       "trieda": "Kožené vyrobky",
       "Nazov": "Kapsa čierna zdobená",
       "cena": "170",
-      "img": "img/Vyrobky z kože/Kapsa 2.png"
+      "img": "https://photos.app.goo.gl/ZajRYu6UDBraKh6G6"
     },
     {
       "id": 10,
       "trieda": "Kožené vyrobky",
       "Nazov": "Kapsa biela zdobená",
       "cena": "139",
-      "img": "img/Vyrobky z kože/Kapsa 3.png"
+      "img": "https://photos.app.goo.gl/VX1xsjiHxABNV1bn9"
     },
     {
       "id": 11,
       "trieda": "Kožené vyrobky",
       "Nazov": "Kapsa hnedá zdobená",
       "cena": "159",
-      "img": "img/Vyrobky z kože/Kapsa.png"
+      "img": "https://photos.app.goo.gl/SqRAompDhZBovEBYA"
     },
     {
       "id": 12,
       "trieda": "Kožené vyrobky",
       "Nazov": "Flaša kožou zdobená 1",
       "cena": "49",
-      "img": "img/Vyrobky z kože/Flasa v koži.png"
+      "img": "https://photos.app.goo.gl/t9KpzWR4YJhBiABR7"
     },
     {
       "id": 13,
       "trieda": "Kožené vyrobky",
       "Nazov": "Byč 1",
       "cena": "60",
-      "img": "img/Vyrobky z kože/byč 2.png"
+      "img": "https://photos.app.goo.gl/RBKgS5RYwqWRg1YS7"
     },
     {
       "id": 14,
       "trieda": "Kožené vyrobky",
       "Nazov": "Byč 2",
       "cena": "60",
-      "img": "img/Vyrobky z kože/byč 3.png"
+      "img": "https://photos.app.goo.gl/6FuMd1RBXXCsJVMc7"
     },
     {
       "id": 15,
       "trieda": "Kožené vyrobky",
       "Nazov": "Opasok",
       "cena": "29",
-      "img": "img/Vyrobky z kože/opasok do sukeniakov pod zadok.png"
+      "img": "https://photos.app.goo.gl/Np65zYBywZMNDLA38"
     },
     {
       "id": 16,
       "trieda": "Kožené vyrobky",
       "Nazov": "Opasok čierny trojprackový",
       "cena": "299",
-      "img": "img/Vyrobky z kože/Opasok.png"
+      "img": "https://photos.app.goo.gl/Gj9PU2YhD1p9hQs58"
     },
     {
       "id": 17,
       "trieda": "Kožené vyrobky",
       "Nazov": "Ozdoba na klobúk",
       "cena": "45",
-      "img": "img/Vyrobky z kože/ozdoba na klobúk.png"
+      "img": "https://photos.app.goo.gl/Fs5BJyctMKZ8coRGA"
     },
     {
       "id": 18,
       "trieda": "Kožené vyrobky",
       "Nazov": "Zápästky",
       "cena": "35",
-      "img": "img/Vyrobky z kože/zapäastky.png"
+      "img": "https://photos.app.goo.gl/xX53wQLZeCHeW6oz5"
     },
     {
       "id": 19,
